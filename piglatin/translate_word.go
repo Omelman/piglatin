2cd464d23142293dc2295b7333d675fa802c474e
danilomelchenko <cr.frog03@gmail.com>
2020-12-16 19:02:08 +0200

    Fix logic

--- piglatin/translate_word.go
+++ piglatin/translate_word.go
@@ -1,18 +1,31 @@
 package piglatin
 
-import "strings"
+import (
+	"strings"
+)
 
 const (
 	suffix          string = "ay"
 	exceptions      string = "aeiou"
-	suffixException        = "d" + suffix
+	SuffixException        = "y" + suffix
 )
 
 func (q *PigLatin) TranslateWord(in string) string {
 	first := in[0:1]
-	if strings.Contains(exceptions, first) {
-		return in + suffixException
+	if strings.Contains(exceptions, strings.ToLower(first)) {
+		if strings.ToLower(in[len(in)-1:]) == "y" {
+			return in + suffix
+		} else {
+			return in + SuffixException
+		}
 	} else {
-		return in[1:] + first + suffix
+		var prefix string
+		for i, letter := range in {
+			if strings.Contains(exceptions, strings.ToLower(string(letter))) {
+				prefix = in[:i]
+				break
+			}
+		}
+		return in[len(prefix):] + prefix + suffix
 	}
 }
